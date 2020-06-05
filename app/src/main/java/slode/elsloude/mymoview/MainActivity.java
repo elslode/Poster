c5f1f60aaed18ef9024f17f62316c119bb08d15d
suminfx <suminfx@mail.ru>
2020-06-05 15:08:09 +0300

    добавил работу witch TopRated or Popularity

--- app/src/main/java/slode/elsloude/mymoview/MainActivity.java
+++ app/src/main/java/slode/elsloude/mymoview/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import org.json.JSONObject;
@@ -18,6 +21,8 @@ import slode.elsloude.mymoview.utils.JSONUtils;
 import slode.elsloude.mymoview.utils.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch switchSort;
     private RecyclerView recyclerViewPostrers;
     private MovieAdapter movieAdapter;
 
@@ -25,12 +30,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switchSort = findViewById(R.id.switchSort);
         recyclerViewPostrers = findViewById(R.id.recyclerViewPosters);
         recyclerViewPostrers.setLayoutManager(new GridLayoutManager(this, 2));
         movieAdapter = new MovieAdapter();
-        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 1);
+        switchSort.setChecked(true);
+        recyclerViewPostrers.setAdapter(movieAdapter);
+        switchSort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setMethedOfSort(isChecked);
+            }
+        });
+        switchSort.setChecked(false);
+    }
+
+    public void onClickSetPopularity(View view) {
+        setMethedOfSort(false);
+    }
+
+    public void onClickSetTopRated(View view) {
+        setMethedOfSort(true);
+    }
+
+    private void setMethedOfSort(boolean isTopRated) {
+        int methodOfSort;
+        if (isTopRated) {
+            methodOfSort = NetworkUtils.TOP_RATED;
+        } else {
+            methodOfSort = NetworkUtils.POPULARITY;
+        }
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort, 1);
         ArrayList<Movie> movies = JSONUtils.getMovieFromJSON(jsonObject);
         movieAdapter.setMovies(movies);
-        recyclerViewPostrers.setAdapter(movieAdapter);
     }
 }
